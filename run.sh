8b0f888e459da5f4e12b024af4413b4773d9c2f8
Joe Zujkowski <jzujkowski@gmail.com>
2019-11-27 14:02:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 if [[ "${LARAVEL}" = true ]]; then
   echo "[info] Running laravel-swoole process"
   cd /code
+  if [[ "${COMPOSER_UPDATE}" == true ]]; then
+    composer update
+  fi
   php artisan swoole:http start
 else
   echo "[info] No run command set.  Running default swoole.php (example app)"
